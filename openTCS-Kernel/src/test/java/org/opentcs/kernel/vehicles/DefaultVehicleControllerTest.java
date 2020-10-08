f8fcf2155577dd9fee93b8b4f3ed51b687bcf7b2
guxiu2233 <13533225019@163.com>
2020-10-08 16:54:15 +0800

    Version 4.1 (2017-04-20) Added functionality for reading and writing the kernel’s plant model file format to the plant overview client.
    Added bezier paths with three control points to the plant overview client.
    
    Added a panel to observe resource allocations to the plant overview client.
    
    Added a dialog requiring user confirmation before changing the driver associated with a vehicle to prevent accidental changes.
    
    Improved performance for transferring model data from the plant overview client to the kernel.
    
    Improved selection of colors used for marking vehicles' routes in the plant overview client.
    
    Improved performance of routing table computation by computing only one table shared by all vehicles by default. (Computation of separate tables for vehicles is optionally possible.)
    
    Many small bugfixes and improvements to code and documentation.
    
    翻译(机翻警告):
    版本4.1（2017-04-20）
    添加了将内核的工厂模型文件格式读写到工厂概览客户端的功能。
    
    向工厂概览客户端添加了带有三个控制点的贝塞尔曲线路径。
    
    添加了一个面板来观察向工厂概览客户端分配的资源。
    
    添加了一个对话框，要求用户确认，然后才能更改与车辆关联的驾驶员，以防止意外更改。
    
    改进了将模型数据从工厂概览客户端传输到内核的性能。
    
    在工厂概览客户端中改进了用于标记车辆路线的颜色选择。
    
    默认情况下，通过仅计算所有车辆共享的一个表，改进了路由表计算的性能。 （可以单独计算车辆的单独表格。）
    
    许多小的错误修正以及对代码和文档的改进。

---
+++ openTCS-Kernel/src/test/java/org/opentcs/kernel/vehicles/DefaultVehicleControllerTest.java
@@ -0,0 +1,227 @@
+/*
+ *
+ * Created on 23.03.2012 09:53:07
+ */
+package org.opentcs.kernel.vehicles;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import net.engio.mbassy.bus.MBassador;
+import net.engio.mbassy.bus.config.BusConfiguration;
+import net.engio.mbassy.listener.Handler;
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import org.opentcs.DataObjectFactory;
+import org.opentcs.access.LocalKernel;
+import org.opentcs.components.kernel.Scheduler;
+import org.opentcs.data.model.Location;
+import org.opentcs.data.model.Path;
+import org.opentcs.data.model.Point;
+import org.opentcs.data.model.Triple;
+import org.opentcs.data.model.Vehicle;
+import org.opentcs.data.order.DriveOrder;
+import org.opentcs.data.order.Route;
+import org.opentcs.drivers.vehicle.LoadHandlingDevice;
+import org.opentcs.drivers.vehicle.VehicleCommAdapter;
+import org.opentcs.drivers.vehicle.VehicleCommAdapterEvent;
+import org.opentcs.drivers.vehicle.VehicleProcessModel;
+import org.opentcs.strategies.basic.scheduling.DummyScheduler;
+
+/**
+ * Test cases for StandardVehicleController.
+ *
+ * @author Philipp Seifert (Fraunhofer IML)
+ * @author Stefan Walter (Fraunhofer IML)
+ */
+public class DefaultVehicleControllerTest {
+
+  private static final String RECHARGE_OP = "recharge";
+  /**
+   * Creates model objects for us.
+   */
+  private final DataObjectFactory dataObjectFactory = new DataObjectFactory();
+  /**
+   * Represents the kernel application's event bus.
+   */
+  private final MBassador<Object> eventBus = new MBassador<>(BusConfiguration.Default());
+  /**
+   * A vehicle.
+   */
+  private Vehicle vehicle;
+  /**
+   * A vehicle model.
+   */
+  private VehicleProcessModel vehicleModel;
+  /**
+   * A (mocked) communication adapter.
+   */
+  private VehicleCommAdapter commAdapter;
+  /**
+   * The (mocked) kernel.
+   */
+  private LocalKernel localKernel;
+  /**
+   * A dummy scheduler.
+   */
+  private Scheduler scheduler;
+  /**
+   * The instance we're testing.
+   */
+  private DefaultVehicleController stdVehicleController;
+
+  @Before
+  public void setUp() {
+    vehicle = dataObjectFactory.createVehicle();
+    vehicleModel = new VehicleProcessModel(vehicle);
+    commAdapter = mock(VehicleCommAdapter.class);
+    localKernel = mock(LocalKernel.class);
+
+    doReturn(RECHARGE_OP).when(commAdapter).getRechargeOperation();
+    doReturn(vehicleModel).when(commAdapter).getProcessModel();
+
+    doReturn(vehicle).when(localKernel).getTCSObject(Vehicle.class, vehicle.getReference());
+    doReturn(vehicle).when(localKernel).getTCSObject(Vehicle.class, vehicle.getName());
+
+    scheduler = new DummyScheduler();
+    scheduler.initialize();
+
+    stdVehicleController = new DefaultVehicleController(vehicle,
+                                                        commAdapter,
+                                                        localKernel,
+                                                        scheduler,
+                                                        eventBus,
+                                                        true);
+    stdVehicleController.initialize();
+  }
+
+  @After
+  public void tearDown() {
+    stdVehicleController.terminate();
+    scheduler.terminate();
+  }
+
+  // Test cases for implementation of interface VehicleManager start here.
+  @Test
+  public void shouldFowardPositionChangeToKernel() {
+    Point point = dataObjectFactory.createPoint();
+    doReturn(point).when(localKernel).getTCSObject(Point.class, point.getName());
+
+    vehicleModel.setVehiclePosition(point.getName());
+
+    verify(localKernel).setVehiclePosition(vehicle.getReference(),
+                                           point.getReference());
+  }
+
+  @Test
+  public void shouldForwardPrecisePositionChangeToKernel() {
+    Triple newPos = new Triple(211, 391, 0);
+    vehicleModel.setVehiclePrecisePosition(newPos);
+
+    verify(localKernel).setVehiclePrecisePosition(vehicle.getReference(),
+                                                  newPos);
+  }
+
+  @Test
+  public void shouldForwardAngleChangeToKernel() {
+    double newAngle = 7.5;
+    vehicleModel.setVehicleOrientationAngle(newAngle);
+
+    verify(localKernel).setVehicleOrientationAngle(vehicle.getReference(),
+                                                   newAngle);
+  }
+
+  @Test
+  public void shouldForwardEnergyLevelChangeToKernel() {
+    int newLevel = 80;
+    vehicleModel.setVehicleEnergyLevel(newLevel);
+    verify(localKernel).setVehicleEnergyLevel(vehicle.getReference(),
+                                              newLevel);
+  }
+
+  @Test
+  public void shouldForwardLoadHandlingDevicesChangeToKernel() {
+    List<LoadHandlingDevice> devices = new LinkedList<>();
+    devices.add(new LoadHandlingDevice("MyLoadHandlingDevice", true));
+    vehicleModel.setVehicleLoadHandlingDevices(devices);
+
+    verify(localKernel).setVehicleLoadHandlingDevices(vehicle.getReference(),
+                                                      devices);
+  }
+
+  @Test
+  public void shouldForwardVehicleStateChangeToKernel() {
+    vehicleModel.setVehicleState(Vehicle.State.EXECUTING);
+
+    verify(localKernel).setVehicleState(vehicle.getReference(),
+                                        Vehicle.State.EXECUTING);
+  }
+
+  @Test
+  public void shouldForwardAdapterStateChangeToKernel() {
+    vehicleModel.setVehicleAdapterState(VehicleCommAdapter.State.UNKNOWN);
+
+    verify(localKernel).setVehicleAdapterState(vehicle.getReference(),
+                                               VehicleCommAdapter.State.UNKNOWN);
+  }
+
+  @Test
+  public void shouldForwardEventToBus() {
+    final String adapterName = "myAdapter";
+    final String eventString = "myString";
+    final List<VehicleCommAdapterEvent> eventsReceived = new LinkedList<>();
+    Object eventHandler = new Object() {
+
+      @Handler
+      public void handleEvent(VehicleCommAdapterEvent event) {
+        eventsReceived.add(event);
+      }
+    };
+    eventBus.subscribe(eventHandler);
+
+    vehicleModel.publishEvent(new VehicleCommAdapterEvent(adapterName, eventString));
+
+    assertEquals("Did not receive exactly one event", 1, eventsReceived.size());
+    VehicleCommAdapterEvent event = eventsReceived.get(0);
+    assertEquals("Received event does not seem to be published event",
+                 eventString,
+                 event.getAppendix());
+  }
+
+  // Test cases for implementation of interface VehicleController start here.
+  @Test
+  public void shouldHaveIdempotentEnabledState() {
+    stdVehicleController.initialize();
+    assertTrue(stdVehicleController.isInitialized());
+    stdVehicleController.initialize();
+    assertTrue(stdVehicleController.isInitialized());
+    stdVehicleController.terminate();
+    assertFalse(stdVehicleController.isInitialized());
+    stdVehicleController.terminate();
+    assertFalse(stdVehicleController.isInitialized());
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldNotAcceptMultipleDriveOrders() {
+    Location location = dataObjectFactory.createLocation();
+    DriveOrder driveOrder = new DriveOrder(new DriveOrder.Destination(
+        location.getReference(), DriveOrder.Destination.OP_NOP));
+
+    Point dstPoint = dataObjectFactory.createPoint();
+    Path stepPath = dataObjectFactory.createPath(dstPoint.getReference());
+    List<Route.Step> steps = Collections.singletonList(
+        new Route.Step(stepPath, null, dstPoint, Vehicle.Orientation.FORWARD, 0));
+
+    driveOrder.setRoute(new Route(steps, 1));
+
+    stdVehicleController.setDriveOrder(driveOrder, new HashMap<>());
+    // Should result in an IllegalStateException:
+    stdVehicleController.setDriveOrder(driveOrder, new HashMap<>());
+  }
+}
